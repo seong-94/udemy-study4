2ebd93a903fc5e7c4213b46223090c74b2c9ef4b
seong-94 <togussla@naver.com>
2024-02-25 23:30:51 +0900

    강의 151

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <main className=" h-screen my-8 flex gap-8">
       <ProjectsSidebar />
       <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
