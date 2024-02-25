c04243d43fa6c28cc3bd7638efd9f44ed4242244
seong-94 <togussla@naver.com>
2024-02-25 21:09:17 +0900

    강의 150

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,15 @@ import Input from "./Input";
 
 export default function NewProject() {
   return (
-    <div>
-      <menu>
+    <div className="w-[35rem] mt-8">
+      <menu className="flex justify-end items-center gap-4 my-4">
         <li>
-          <button>Cancel</button>
+          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
         </li>
         <li>
-          <button>Save</button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
         </li>
       </menu>
       <div>
