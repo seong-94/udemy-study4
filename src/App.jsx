4e6cf3cce4bb1bd5ba6c8c7dbd679712c8f723f5
seong-94 <togussla@naver.com>
2024-02-25 23:49:03 +0900

    강의 152

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projects, setProjects] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjects((prevProjects) => {
+      return {
+        ...prevProjects,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+  if (projects.selectedProjectId === null) {
+    content = <NewProject onStartAddProject={handleStartAddProject} />;
+  } else {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className=" h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {/* <NewProject /> */}
+      {content}
+      {/* <NoProjectSelected onStartAddProject={handleStartAddProject} /> */}
     </main>
   );
 }
